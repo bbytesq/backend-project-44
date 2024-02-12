32fcd95e2e8a0e030571372e12a83f71fdfebb71
Vasiliy <pauli.b99@mail.ru>
2024-02-12 15:58:53 +0700

    123

---
+++ bin/brain-gcd.js
@@ -0,0 +1,39 @@
+import readlineSync from "readline-sync";
+import hello, { getRandomInt } from "../src/cli.js";
+import { name } from "../src/cli.js";
+hello();
+let r;
+let counter = 0;
+export const gcd = (a, b) => {
+    while (a !== b) {
+        if (a > b) {
+          a = a - b;
+        }
+        else {
+          b = b - a;
+        }
+      }
+    return a;
+};
+  const gcdGame = () => {
+    const num1 = getRandomInt(100);
+    const num2 = getRandomInt(100);
+    const correctGcd = gcd(num1, num2);
+    console.log(`Question: ${num1} ${num2}`)
+    r = parseInt(readlineSync.question("Your answer: "));
+    if (r === correctGcd) {
+      console.log("Correct!");
+      counter += 1;
+    } else {
+      console.log(`${r} is wrong answer ;(, Correct answer: ${correctGcd}`);
+    }
+    console.log(counter);
+    if (counter > 0 && counter < 3) {
+      gcd();
+    }
+  };
+gcdGame();
+if (counter === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+  export default gcdGame;
