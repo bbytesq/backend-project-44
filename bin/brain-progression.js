2b7460bfb217c3215730c749ba16c7e4a54e02b1
Vasiliy <pauli.b99@mail.ru>
2024-02-13 11:45:56 +0700

    version5

---
+++ bin/brain-progression.js
@@ -0,0 +1,36 @@
+import readlineSync from "readline-sync";
+import hello, { getRandomInt } from "../src/cli.js";
+import { name } from "../src/cli.js";
+
+hello();
+let count = 0;
+const progressionVal = (answer, trueVal) => {
+    if (answer === trueVal) {
+        console.log('Correct!');
+        count += 1;
+    } else {
+        count += 10;
+        console.log(`${answer} is wrong answer ;(. Correct answer was ${trueVal}.\nLet's try again, ${name}`);
+    }
+};
+while (count < 3) {
+    const arr = [];
+    let firstProgressionVal = getRandomInt(100);
+    const progressionDiff = getRandomInt(10);
+    while (arr.length !== 10) {
+        const result = progressionDiff + firstProgressionVal;
+        arr.push(result);
+        firstProgressionVal = result;
+    }
+    const index = getRandomInt(9);
+    const trueVal = arr[index - 1] + (arr[index + 1] - arr[index - 1]) / 2;
+    arr[index] = '..';
+    console.log(`Question: ${arr}`);
+    const answer = parseInt(
+        readlineSync.question('Your answer: ') || 'no answer'
+    );
+    progressionVal(answer, trueVal);
+    if (count === 3) {
+        console.log(`Congratulations, ${name}!`);
+    }
+}
