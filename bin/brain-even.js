e11b0402cd9cb4de41b87e1386583f63b0e6964e
Vasiliy <pauli.b99@mail.ru>
2024-02-26 13:01:49 +0700

    43232

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-import readlineSync, { question } from 'readline-sync';
 import hello from '../src/cli.js';
-import {name, getRandomInt, } from '../src/cli.js';
 import isEven from '../src/games/brain-even.js';
 hello()
 isEven()
